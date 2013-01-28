49fa4a5ddfcdf6b94ffd1f4a5c47bfc0ad5ded1d
Kenny Aneke <weytin@live.com>
2013-01-28 22:06:51 +0100

    Updated the abstract functions.

--- engine/model/drivers/MySQLi/mysqli_library.php
+++ engine/model/drivers/MySQLi/mysqli_library.php
@@ -2,7 +2,7 @@
 /*
  * MySQLi Library - EasyCode
  *
- * @author Ekene Aneke
+ * @author Weytin
 */
 
 abstract class MySQLi_Library
@@ -10,7 +10,5 @@ abstract class MySQLi_Library
 	abstract protected function dbConnect();	// Calling the dbConnect(); function
 	abstract protected function dbDisconnect();	// Calling the dbDisconnect(); function
 	abstract protected function dbReconnect();	// Calling the dbReconnect(); function
-	abstract protected function cleanString();	// Calling the cleanString(); function
-	abstract protected function secureString();	// Calling the secureString(); function
 }
 ?>
