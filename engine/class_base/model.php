429d5b587c49318492a9492993d1e11980a8edf3
Kenny Aneke <weytin@live.com>
2013-01-28 21:58:23 +0100

    Created connection to RedBeanPHP

--- engine/class_base/model.php
+++ engine/class_base/model.php
@@ -8,6 +8,21 @@
 if(!defined('INDEX')) { die('This file contains valid information, which you cannot view.'); }
 class Model
 {
-
+  public function __construct()
+  {
+		$connect = R::setup('mysql:host=' .Config::read('sql.host'). ';dbname=' .Config::read('sql.db'). '',Config::read('sql.user'), Config::read('password'));
+		return $connect;
+	}
+	
+	public function insert()
+	{
+		$rows = array(
+		'random' => 'random data',
+		'2 random' => '2 randome data'
+		);
+		
+		$query = "INSERT INTO users (`" .implode(',', $rows). "`)";
+		$query2 = "VALUES ('" .implode(',', $rows). "')";
+	}
 }
 ?>
